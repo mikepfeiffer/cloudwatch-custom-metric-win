90825b62f3c2cec778e0b0236bf161b5ff5c8a82
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2016-05-08 20:35:57 +0000

    initial commit

---
+++ Send-SystemDiskFreePercentage.ps1
@@ -0,0 +1,30 @@
+﻿param(
+    [Parameter(Position=0, ValueFromPipelineByPropertyName=$true, Mandatory=$false)]
+    [String]
+    $Name = $env:COMPUTERNAME,
+
+    [switch]
+    $ViewOnly
+)
+  
+process {
+    $free = Get-Counter "\LogicalDisk(c:)\% Free Space" -comp $Name
+
+    $result = $free.CounterSamples | ?{$_.InstanceName -match ":"} | 
+        select @{n="Server";e={$Name}},
+               @{n="Drive";e={$_.InstanceName}},
+	           @{n="PercentFree";e={[Math]::Round($_.CookedValue,2)}}
+    
+    if($ViewOnly) {
+        Write-Output $result
+    }
+    else {
+        $instanceId = Invoke-RestMethod http://169.254.169.254/latest/meta-data/instance-id
+        $dimensions = "Name=InstanceId,Value=$instanceId"
+
+        aws cloudwatch put-metric-data --metric-name RootVolPercentFree `
+        --namespace Windows `
+        --dimensions $dimensions `
+        --value $result.PercentFree    
+    }
+}
