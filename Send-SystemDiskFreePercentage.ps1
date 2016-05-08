38021b75b76553ba04cc133c0273bfebc7bbbe75
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2016-05-08 13:45:12 -0700

    added write-verbose statements

--- Send-SystemDiskFreePercentage.ps1
+++ Send-SystemDiskFreePercentage.ps1
@@ -1,4 +1,5 @@
-﻿param(
+﻿[CmdletBinding()]
+param(
     [Parameter(Position=0, ValueFromPipelineByPropertyName=$true, Mandatory=$false)]
     [String]
     $Name = $env:COMPUTERNAME,
@@ -8,6 +9,7 @@
 )
   
 process {
+    Write-Verbose "Gathering performance monitor data"
     $free = Get-Counter "\LogicalDisk(c:)\% Free Space" -comp $Name
 
     $result = $free.CounterSamples | ?{$_.InstanceName -match ":"} | 
@@ -16,15 +18,19 @@ process {
 	           @{n="PercentFree";e={[Math]::Round($_.CookedValue,2)}}
     
     if($ViewOnly) {
+        Write-Verbose "ViewOnly switch used...outputting results"
         Write-Output $result
     }
     else {
+        Write-Output "Retrieving instance id from EC2 instance meta-data"
         $instanceId = Invoke-RestMethod http://169.254.169.254/latest/meta-data/instance-id
+
         $dimensions = "Name=InstanceId,Value=$instanceId"
 
+        Write-Verbose "Publishing disk free space percentage via put-metric-data"
         aws cloudwatch put-metric-data --metric-name RootVolPercentFree `
         --namespace Windows `
         --dimensions $dimensions `
-        --value $result.PercentFree    
+        --value $result.PercentFree
     }
 }
